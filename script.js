1981542261dc90cc5a5773824c5b331178e11be3
tarekmonowar <tarekmonowar2332@gmail.com>
2024-07-09 02:22:51 +0600

    full responsive

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ var navUlPar = document.getElementById ("navUlPar")
 function showmenu() {
 
     navUlPar.style.right="0";
+   
+
 }
 
 function hidemenu(){
     navUlPar.style.right="-200px";
+    
 }
